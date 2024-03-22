bfeba2ea2a30353acb7f1d9133dcf49b23909a8e
Kenntechnocrat <oumakenn21@gmail.com>
2024-03-22 00:22:44 -0700

    first_ass

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input('Enter your name:')
+age=input('Enter your age:')
+location=input('Enter your location:')
+print('Bonjour, my name is',name ,',I am ',age,' years old and I hail from',location)
